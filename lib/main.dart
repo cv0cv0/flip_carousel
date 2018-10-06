90256182b0608a4c08ccac558dde5498c1c895f0
cv0cv0 <linux2.0@foxmail.com>
2018-10-06 09:06:46 +0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'page/home_page.dart';
 
-void main() => runApp(App());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(App());
+}
 
 class App extends StatelessWidget {
   @override
